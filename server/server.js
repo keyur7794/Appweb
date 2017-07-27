210b5171750c2418318da28a723e703b15a738b4
keyur panchal <keyur4862@gmail.com>
2017-07-27 16:05:39 +0530

    'initcommit'

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path=require('path');
+const publicpath =path.join(__dirname,'../public');
+console.log(__dirname+'/../public');
+console.log(publicpath);
+
+const express =require('express');
+const port=process.env.PORT||3000;
+
+var app=express();
+app.use(express.static(publicpath));
+
+app.listen(port,()=>console.log(`server is on port ${port}`));
