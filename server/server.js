39ef883025dbcd3fd4c1a82f5c64223882e5c42f
keyur panchal <keyur4862@gmail.com>
2017-07-28 02:07:57 +0530

    'addconnectAndDisconnect'

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path=require('path');
+const http=require('http');
 const publicpath =path.join(__dirname,'../public');
 console.log(__dirname+'/../public');
 console.log(publicpath);
 
 const express =require('express');
+const socketIO=require('socket.io');
+
 const port=process.env.PORT||3000;
 
 var app=express();
+var server =http.createServer(app);
+var io=socketIO(server);
+
+io.on('connection',(socket)=>{
+  console.log('new user connected');
+
+  socket.on('disconnect',()=>{
+    console.log('user was disconnect');
+  });
+
+
+});
 app.use(express.static(publicpath));
 
-app.listen(port,()=>console.log(`server is on port ${port}`));
+server.listen(port,()=>console.log(`server is on port ${port}`));
