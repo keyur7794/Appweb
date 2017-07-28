dc8c38872db70512c00b9dcbdc0a4f8dfc7300b1
keyur panchal <keyur4862@gmail.com>
2017-07-29 01:11:16 +0530

    'BroadcastEvent'

--- public/js/index.js
+++ public/js/index.js
@@ -3,14 +3,6 @@
 
    socket.on('connect',function(){
      console.log('connected to server');
-
-     socket.emit('createMessage',
-   {
-     from:'keyur',
-     text:'hello',
-     createAt:'123456'
-   });
-
    });
   socket.on('disconnect',function(){
     console.log('disconnect');
--- server/server.js
+++ server/server.js
@@ -16,15 +16,18 @@ var io=socketIO(server);
 io.on('connection',(socket)=>{
   console.log('new user connected');
 
-  socket.emit('newMessage',
-{
-  from:'keyur',
-  text:'hello',
-  createAt:'123456'
-});
+
 
 socket.on('createMessage',(message)=>{
   console.log('createMessage',message);
+
+io.emit('newMessage',{
+from:message.from,
+text:message.text,
+createdAt:new Date().getTime()
+
+});
+
 });
 
 
