fd32e01b7b766f82a7598a7f2875876ff748bc97
keyur panchal <keyur4862@gmail.com>
2017-07-31 12:25:55 +0530

    'genMessgeUtility'

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http=require('http');
 const publicpath =path.join(__dirname,'../public');
 console.log(__dirname+'/../public');
 console.log(publicpath);
-
+const {generateMessage}=require('./utils/message');
 const express =require('express');
 const socketIO=require('socket.io');
 
@@ -16,17 +16,18 @@ var io=socketIO(server);
 io.on('connection',(socket)=>{
   console.log('new user connected');
 
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
+
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
 socket.on('createMessage',(message)=>{
   console.log('createMessage',message);
 
-io.emit('newMessage',{
-from:message.from,
-text:message.text,
-createdAt:new Date().getTime()
+io.emit('newMessage',generateMessage(message.from,message.text));
 
-});
+
+
 
 });
 
