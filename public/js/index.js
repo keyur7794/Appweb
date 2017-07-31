b67696cd68f511cf5be4f6977011c70618b10c32
keyur panchal <keyur4862@gmail.com>
2017-07-31 13:34:00 +0530

    'AddForm'

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,27 @@
 
   socket.on('newMessage',function(message){
     console.log('new message',message);
+    var li=jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    jQuery('#messages').append(li);
+
+
   });
+
+  socket.emit('createMessage',{
+    from:'frank',
+    text:'hi'
+  },function(data){
+console.log('got it',data);
+
+  });
+
+jQuery('#message-form').on('submit',function(e){
+e.preventDefault();
+
+socket.emit('createMessage',{
+  from:'user',
+  text:jQuery('[name=message]').val()
+},function(){
+});
+});
