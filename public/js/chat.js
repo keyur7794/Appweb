2b7280abef43752926f4135609781aeaaf34712f
keyur panchal <keyur4862@gmail.com>
2017-08-04 04:20:59 +0530

    'joinpage'

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,27 @@
 
    var socket=io();
+   function scrollToBottom() {
+
+
+     var messages=jQuery('#messages');
+     var newMessage=messages.children('li:last-child');
+
+     console.log(newMessage);
+
+     var clientHeight=messages.prop('clientHeight');
+     var scrollTop=messages.prop('scrollTop');
+     var scrollHeight =messages.prop('scrollHeight');
+     var newMessageHeight=newMessage.innerHeight();
+     var lastMessageHeight=newMessage.prev().innerHeight();
+
+
+     if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+     {
+        messages.scrollTop(scrollHeight);
+     }
+
+   }
+
 
    socket.on('connect',function(){
      console.log('connected to server');
@@ -19,10 +41,10 @@
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
-    // var li=jQuery('<li></li>');
-    // li.text(`${message.from} ${formattedTime}:${message.text}`);
-    // jQuery('#messages').append(li);
+
+
 
 
   });
@@ -46,14 +68,7 @@ socket.on('newLocationMessage',function(message){
       });
 
         jQuery('#messages').append(html);
-
-// var li=jQuery('<li></li>');
-// var a=jQuery('<a target="_blank"> My current location</a>');
-//
-// li.text(`${message.from}:${formattedTime}  `);
-// a.attr('href',message.url);
-// li.append(a);
-// jQuery('#messages').append(li);
+        scrollToBottom();
 
 });
 
